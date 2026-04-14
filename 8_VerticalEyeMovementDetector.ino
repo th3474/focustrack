ccbe08c5cc21d02a11cb4b906a526830a5917c76
th3474 <th3474@nyu.edu>
2026-04-13 20:26:57 -0400

    Iterration 1
    Refactor vertical eye movement detection logic to include a second sensor and improve direction detection.

--- 8_VerticalEyeMovementDetector.ino
+++ 8_VerticalEyeMovementDetector.ino
@@ -1,9 +1,16 @@
+//graph for up and down - done
+//
+
 int range1 = 0;
+int range2;
 int lastDirection = 0;
 
 int cycleCount = 0;
 unsigned long startTime = 0;
-const unsigned long windowTime = 10000; // 10 sec
+const unsigned long windowTime = 10000;  // 10 sec
+
+float alpha = 0.1;
+float filtered = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -11,39 +18,55 @@ void setup() {
 }
 
 void loop() {
-int sensor1 = analogRead(A0);
-int constrainedInput1 = constrain(sensor1, 350, 650);
-range1 = map(constrainedInput1, 350, 650, 0, 6);
+  delay(200);
+  float sensor1 = analogRead(A0);
+  float sensor2 = analogRead(A1);
+  // filtered = alpha*sensor1+(1-alpha)*filtered;
+  // Serial.println(filtered);
 
-switch (range1) {
-case 0:
-  if (lastDirection != -1) {
-    Serial.println("Down");
-  if (lastDirection == 1) {
-    cycleCount++;
-  }
-    lastDirection = -1;}
-break;
+  int constrainedInput1 = constrain(sensor1, 350, 650);
+  int constrainedInput2 = constrain(sensor2, 350, 650);
 
-case 4:
-  if (lastDirection != 1) {
-  Serial.println("Up");lastDirection = 1;
-  }
+  range1 = map(constrainedInput1, 350, 650, 0, 6);
+  range2 = map(constrainedInput2, 350, 650, 0, 6);
+  switch (range1) {
+  case 0:
+    if (lastDirection != -1) {
+      Serial.println("Down");
+      lastDirection = -1;
+      }
   break;
 
-  default:
-  Serial.println("0");
-  break;
-}
+  case 4:
+    if (lastDirection != 1) {
+      Serial.println("Up");
+      lastDirection = 1;
+    }
+      break;
 
-if (millis() - startTime >= windowTime) {float frequency = cycleCount / (windowTime / 1000.0);
-Serial.print("Frequency = ");
-Serial.print(frequency);
-Serial.println(" Hz");
-
-cycleCount = 0;
-startTime = millis();
-}
-
-delay(50);
+    default:
+    Serial.println("0");
+    break;
   }
+
+  switch (range2) {
+  case 0:
+    if (lastDirection != -1) {
+      Serial.println("Left");
+      lastDirection = -1;
+      }
+  break;
+
+  case 4:
+    if (lastDirection != 1) {
+      Serial.println("Right");
+      lastDirection = 1;
+    }
+      break;
+
+    default:
+    Serial.println("1");
+    break;
+  }
+
+}
